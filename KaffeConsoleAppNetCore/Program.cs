1e3015e3282e9ce17b511ce3a9dcebdee04592c1
Mads Meinert Andersen <mads485d@edu.easj.dk>
2019-02-02 22:09:14 +0100

    opg 1-5

--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -7,11 +8,33 @@ namespace KaffeConsoleAppNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
+            List<Kaffe> kaffeList = new List<Kaffe>();
             
+            Cortado cortado = new Cortado();
 
-           
+            FlatWhite flatWhite = new FlatWhite(20);
+
+            Latte latte = new Latte();
+
+            SortKaffe sortKaffe = new SortKaffe(10);
+
+            kaffeList.Add(cortado);
+            kaffeList.Add(flatWhite);
+            kaffeList.Add(latte);
+            kaffeList.Add(sortKaffe);
+            Console.WriteLine("KAFFER:");
+            foreach (var hKaffe in kaffeList)
+            {
+                Console.WriteLine($"Navn : {hKaffe.ToString()} Pris : {hKaffe.Pris()} kr. Styrke : {hKaffe.Styrke()}");
+                Console.WriteLine();
+            }
+
+
+
+
+            Console.ReadKey();
+
 
         }
     }
